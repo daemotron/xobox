7bb7414bb4926442ae818d311ab8bfcb05eb20bd
daemotron <mail@daemotron.net>
2018-11-02 13:46:07 +0100

    new ui skeleton and entry point

---
+++ xobox/ui/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+    xobox.ui.main
+    ~~~~~~~~~~~~~
+    :copyright: Copyright 2018 by the Stormrose Project team, see AUTHORS.
+    :license: MIT License, see LICENSE for details.
+"""
+
+
+import sys
+
+
+def main() -> int:
+    """
+    xobox main function
+
+    Central entry point for xobox. This function evaluates command
+    line arguments, prepares and executes the ui and ensures clean
+    exit behaviour.
+    """
+    return 0
